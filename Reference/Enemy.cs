38e4cd99eaf860cdbdbf9222dcf9244fbb8e4b41
Mikael <mikael.bergstrom@ga.ntig.se>
2020-09-29 13:17:10 +0200

    Initial commit

---
+++ Reference/Enemy.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Reference
+{
+    public class Enemy
+    {
+      public int hp = 100;
+    }
+}
---
+++ Reference/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Reference
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int y = 100;
+
+      int u = y;
+
+      int[] g = {100};
+      int[] b = g;
+
+      Enemy e1 = new Enemy();
+
+      DoThing(e1);
+
+      Console.WriteLine(e1.hp);
+
+      Console.ReadLine();
+
+    }
+
+    static void DoThing(Enemy k)
+    {
+      k.hp++;
+    }
+  }
+}
